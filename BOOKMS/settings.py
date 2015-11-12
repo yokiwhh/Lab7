cc9e946a406e49bbf801f3f05eb15cfa9a87cf19
yokiwhh <2968070824@qq.com>
2015-11-12 21:47:59 +0800

    added new benchmarks1

--- BOOKMS/settings.py
+++ BOOKMS/settings.py
@@ -125,9 +125,9 @@ INSTALLED_APPS = (
     'bookapp',
     'django-groundwork',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
